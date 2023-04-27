c4a8f4301983c5ad7b184ff18b68d506cf671f7d
Faith-vaati <vaati.faith79@gmail.com>
2023-04-28 00:41:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [37, -1.2921],
+    zoom: 6,
+    projection: "EPSG:4326"
+  })
+});
